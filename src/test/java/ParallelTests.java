4f0019ad931bc4a5058c14c6ef4ef7fb99c52b3a
lilinor <ekwok@genymobile.com>
2018-08-31 17:03:24 +0200

    Add video test recording

--- src/test/java/ParallelTests.java
+++ src/test/java/ParallelTests.java
@@ -1,20 +1,25 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.screenrecording.ScreenRecordingUploadOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 public class ParallelTests {
 
     private final static String APPIUM_SERVER_URL = "http://localhost:4723/wd/hub";
+    private final static String PATH_VIDEO_FILE = "/Users/ekwok/Documents/Appium/SeeTest/Videos/video_"; //will add the device udid at the end
+
     private AndroidDriver driver;
 
     @BeforeTest(alwaysRun = true)
@@ -37,6 +42,16 @@ public class ParallelTests {
         // Setup implicit wait to 10 seconds to wait for mobile elements.
         // Use a higher value if your mobile elements take time to show up
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        ScreenRecordingUploadOptions uOpt = new ScreenRecordingUploadOptions().withRemotePath(PATH_VIDEO_FILE);
+
+        AndroidStartScreenRecordingOptions rOpt = new AndroidStartScreenRecordingOptions().
+                withUploadOptions(uOpt);
+
+        driver.startRecordingScreen(rOpt);
     }
 
     @Test
@@ -68,8 +83,27 @@ public class ParallelTests {
     }
 
     @AfterTest
-    public void tearDown() throws Exception {
+    @Parameters("udid")
+    public void tearDown(String udid) throws Exception {
+
+        String result = driver.stopRecordingScreen();
+        decoder(result, PATH_VIDEO_FILE+udid+".mp4");
+
         driver.quit();
+    }
+
+    private static void decoder(String base64, String pathFile) {
+        try {
+            FileOutputStream imageOutFile = new FileOutputStream(pathFile);
+            // Converting a Base64 String into Image byte array
+            byte[] ByteArray = Base64.getMimeDecoder().decode(base64);
+            System.out.println(ByteArray);
+            imageOutFile.write(ByteArray);
+        } catch (FileNotFoundException e) {
+            System.out.println("Image not found" + e);
+        } catch (IOException ioe) {
+            System.out.println("Exception while reading the Image " + ioe);
+        }
     }
 
 
